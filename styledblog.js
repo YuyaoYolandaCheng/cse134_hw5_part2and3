d6c02cc082c054ea573cae700ffbe0b671df60af
YuyaoYolandaCheng <56518914+YuyaoYolandaCheng@users.noreply.github.com>
2021-03-10 02:59:16 -0800

    Update styledblog.js

--- styledblog.js
+++ styledblog.js
@@ -61,8 +61,8 @@ add.onclick = function(){
         } 
         
         var user_info = localStorage.getItem("gotrue.user");
-        var split1 = user_info.split(",");
-        var split2 = split1[1];
+        var split1 = user_info.split("\"");
+        var split2 = split1[3];
         console.log(split2);
 
         var new_table = document.getElementById("crudlist").getElementsByClassName("content")[0];
